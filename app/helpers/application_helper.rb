b812b72de280550bee341bedc7295048395bf02a
kobby08 <linusquam@gmail.com>
2021-06-20 19:20:40 +0000

    Adding store(catalog page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def current_time
+    time = Time.now
+    time.strftime('%a %B, %Y %H:%M ')
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  # validations
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title,
+            length: {
+              minimum: 5,
+              message: 'Title length must be at least 5',
+            }
+  validates :image_url,
+            allow_blank: true,
+            format: {
+              with: /\.(gif|jpg|png)\z/i,
+              message: 'must be of type .gif, .jpg or .png',
+            }
 end
