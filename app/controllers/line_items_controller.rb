16f05198b362ffd554773fa9afafaf44fff2c464
kobby08 <linusquam@gmail.com>
2021-06-25 03:14:37 +0000

    Creating shopping cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,8 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: %i[ show edit update destroy ]
+  include CurrentCart
+
+  before_action :set_line_item, only: %i[show edit update destroy]
+  before_action :set_cart, only: [:create]
 
   # GET /line_items or /line_items.json
   def index
@@ -7,8 +10,7 @@ class LineItemsController < ApplicationController
   end
 
   # GET /line_items/1 or /line_items/1.json
-  def show
-  end
+  def show; end
 
   # GET /line_items/new
   def new
@@ -16,20 +18,25 @@ class LineItemsController < ApplicationController
   end
 
   # GET /line_items/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: "Line item was successfully created." }
+        format.html do
+          redirect_to @line_item.cart,
+                      notice: 'Line item was successfully created.'
+        end
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @line_item.errors, status: :unprocessable_entity
+        end
       end
     end
   end
@@ -38,11 +45,15 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: "Line item was successfully updated." }
+        format.html do
+          redirect_to @line_item, notice: 'Line item was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @line_item.errors, status: :unprocessable_entity
+        end
       end
     end
   end
@@ -51,19 +62,23 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html do
+        redirect_to line_items_url,
+                    notice: 'Line item was successfully destroyed.'
+      end
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id, :cart_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails
   .draw do
     root to: 'store#index', as: 'store_index'
     resources :products
+    resources :carts
+    resources :line_items
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
